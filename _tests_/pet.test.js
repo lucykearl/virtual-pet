b1f4fc8d31ef2fe1b78be7fd47c91027a4c27691
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-06-18 15:39:23 +0100

    Test for getting older and getting unhealthier

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -4,21 +4,29 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-});
 
-describe('constructor', () => {
     it('sets the name property', () => {
         const pet = new Pet('Fido');
 
         expect(pet.name).toEqual('Fido');
     });
-});
 
-describe('constructor', () => {
     it('has an inital age of 0', () => {
         const pet = new Pet('Fido');
 
         expect(pet.age).toEqual(0);
+    });
+
+    it('has an inital hunger of 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it('has an inital fitness of 10', () => {
+        const pet = new Pet('Fido')
+
+        expect(pet.fitness).toEqual(10);
     });
 });
 
@@ -30,4 +38,25 @@ describe('growUp', () => {
 
         expect(pet.age).toEqual(1);
     });
+
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it('decreases the fitness by 3', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    });
 });
+
+
+
+
+
