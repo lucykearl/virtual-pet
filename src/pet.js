e3ca58137d4f7c1a8b23570c92db804536dcd345
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-06-28 20:48:56 +0100

    Code for checkup

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,10 @@
+const minFitness = 0;
+const maxFitness = 10;
+const minHunger = 0;
+const maxHunger = 10;
+const hungerBreakPoint = 5;
+const fitnessBreakPoint = 3;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -10,5 +17,39 @@ Pet.prototype.growUp = function() {
     this.hunger += 5;
     this.fitness -= 3;
 };
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= 10 ) {
+        this.fitness += 4;
+    } else {
+        this.fitness = 10;
+    }
+};
+
+Pet.prototype.feed = function() {
+    this.hunger -= 3;
+    if (this.hunger < minHunger){
+        this.hunger = minHunger;
+    }
+
+Pet.prototype.checkUp = function() {
+    if (this.fitness <= fitnessBreakPoint){
+        return 'I need a walk';
+    }
+
+    if (this.hunger >= hungerBreakPoint){
+        return 'I am hungry';
+    }
+
+    if (this.fitness <= fitnessBreakPoint && this.hunger >= hungerBreakPoint){
+        return 'I am hungry AND I need a walk';
+    }
+
+    else {
+        return 'I feel great';
+    }
+};
+}
+
 
     module.exports = Pet;
